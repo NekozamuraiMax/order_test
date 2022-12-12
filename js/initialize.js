9173db2e0b720ca2a4673f2dccbcbde53c51463b
NekozamuraiMax <118565220+NekozamuraiMax@users.noreply.github.com>
2022-12-12 22:52:01 +0900

    Update initialize.js

--- js/initialize.js
+++ js/initialize.js
@@ -53,7 +53,14 @@ $(function(){
 		const date  = document.getElementById("datepicker").value;
 		const time  = document.getElementById("scheduled-time").value;
 		const freetxt= document.getElementById("textarea").value;
-		const message= genre + ',' + date + ',' + time + ',' + freetxt;
+		const message;
+		if(genre=="reserve"){
+			message = '[申請内容]予定の追加\n'+'[指定日]'+date+'\n'+'[時間]'+time+'\n'+'[伝達事項]\n'+freetxt;
+		}else if(genre=="cancel"){
+			message = '[申請内容]キャンセル\n'+'[指定日]'+date+'\n'+'[伝達事項]\n'+freetxt;
+		}else if(genre=="change"){
+			message = '[申請内容]時間の変更\n'+'[指定日]'+date+'\n'+'[時間]'+time+'\n'+'[伝達事項]\n'+freetxt;
+		}
 		sendText(message);
 		return false;
 	});
