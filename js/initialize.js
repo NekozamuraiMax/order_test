ded8146c6b003bc98859be1490ca4be51159c170
NekozamuraiMax <118565220+NekozamuraiMax@users.noreply.github.com>
2023-06-27 11:54:51 +0900

    Update initialize.js

--- js/initialize.js
+++ js/initialize.js
@@ -63,7 +63,12 @@ $(function(){
 	$('form').submit(function(){
   		let res = $('form').serialize();
 		$.post('https://script.google.com/macros/s/AKfycbyCiL0cL_JPghM1ufI60IJAP0d7LFTXYDPnxoNORNbqwf4ggfZoW_v00mOurCYmRV9_mA/exec', res);
-		liff.closeWindow();
+		$('#splash').addClass("close");
+		$('splash').delay(1200).fadeIn('slow', function(){
+			$('#splash-end-logo").style.display = "block";
+		});
+		
+		setTimeout(liff.closeWindow(), 3000);
 		return false;
 	});
 });
