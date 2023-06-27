7f085a8dd6f475eac34524ff146068f156dfa4b7
NekozamuraiMax <118565220+NekozamuraiMax@users.noreply.github.com>
2023-06-27 12:26:19 +0900

    Update initialize.js

--- js/initialize.js
+++ js/initialize.js
@@ -63,7 +63,8 @@ $(function(){
 	$('form').submit(function(){
   		let res = $('form').serialize();
 		$.post('https://script.google.com/macros/s/AKfycbyCiL0cL_JPghM1ufI60IJAP0d7LFTXYDPnxoNORNbqwf4ggfZoW_v00mOurCYmRV9_mA/exec', res);
-		$('#splash').addClass('close');
+		$('#splash-end-logo').style.display = "block";
+		$('#splash').delay(1200).fadeIn('slow');
 		setTimeout(liff.closeWindow, 3000);
 		return false;
 	});
