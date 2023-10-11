692f9dc0ed7f4e0049fc443886ce7a0fde704637
NekozamuraiMax <118565220+NekozamuraiMax@users.noreply.github.com>
2023-10-11 13:46:06 +0900

    Update initialize.js

--- js/initialize.js
+++ js/initialize.js
@@ -17,7 +17,7 @@ window.onload = function(e){
 	});
 };
 
-const name = params.get('name').toString();;
+const name = params.get('name').toString();
 function initializeApp() {
     // ログインチェック
     if (liff.isLoggedIn()) {
@@ -62,7 +62,7 @@ function sendText(text){
 $(function(){
 	$('form').submit(function(){
   		let res = $('form').serialize();
-		$.post('https://script.google.com/macros/s/AKfycbyczmHXpRWUcaeXp9H_r7oztBeC_76gQS3Kn26YUAjuZZSp6DnQ4kGTHWkWitvP6hz9Hw/exec', res);
+		$.post('https://script.google.com/macros/s/AKfycby7DZDBDMmq5gm5-HV-Lc_xHD-0umcyYc7NqzX27G0Kp0jJf4ne8euMZwtD2mDGwUtZdQ/exec', res);
 		//$.post('https://script.google.com/macros/s/AKfycbw3pUQJmgpIGQzkAPQKZLP0tftiSQHdGYh_XRzblnt97DDyczb-k5YKuxT5icz315bIfw/exec', res);
 		$('#splash').delay(1000).fadeIn('slow', function(){
 			$('#splash-end-logo').fadeIn('slow');
