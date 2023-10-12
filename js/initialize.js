6078a965b220721618b832b01d68c1472c1bd16b
NekozamuraiMax <118565220+NekozamuraiMax@users.noreply.github.com>
2023-10-12 11:02:30 +0900

    Update initialize.js

--- js/initialize.js
+++ js/initialize.js
@@ -62,7 +62,7 @@ function sendText(text){
 $(function(){
 	$('form').submit(function(){
   		let res = $('form').serialize();
-		$.post('https://script.google.com/macros/s/AKfycbyLoTfGVP6TawrseJgwK_uVyehcGXKlm4tPMR7RC-hT7QaE6DM9c1du0htpv4O3-uTLdQ/exec', res);
+		$.post('https://script.google.com/macros/s/AKfycbzfS5P9dPoYUxUPc8tpza7sItyIAGG0jsPGHtkcu9vAKR3umEJ_AHifaJlcuH6LdOJ-Tw/exec', res);
 		//$.post('https://script.google.com/macros/s/AKfycbz25b_XtOPuwGGizMAfiZHfxunzDI-hO2qhuWuf3BQbU8yXdqBhGFYUvZlyz1KRiveRKg/exec', res);
 		$('#splash').delay(1000).fadeIn('slow', function(){
 			$('#splash-end-logo').fadeIn('slow');
